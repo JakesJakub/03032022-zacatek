432c7507134ad346d54f9f9e7849a1f6aa9f8c65
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-03-17 11:12:34 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ radio.set_transmit_power(3)
 radio.set_group(111)
 RunComp.set_light_level()
 
+reset = 0
+zacatek = 1
+stop = 2
+
 def on_light_drop():
-    radio.send_number(1)
+    radio.send_number(zacatek)
 RunComp.on_light_drop(on_light_drop)
+
+def on_button_pressed_a():
+    radio.send_number(reset)
+    control.reset()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_received_number(stop):
+    control.reset()
+radio.on_received_number(on_received_number)
--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 radio.setTransmitPower(3)
 radio.setGroup(111)
 RunComp.SetLightLevel()
+let reset = 0
+let zacatek = 1
+let stop = 2
 RunComp.OnLightDrop(function on_light_drop() {
-    radio.sendNumber(1)
+    radio.sendNumber(zacatek)
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    radio.sendNumber(reset)
+    control.reset()
+})
+radio.onReceivedNumber(function on_received_number(stop: number) {
+    control.reset()
 })
